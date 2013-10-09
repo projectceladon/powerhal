f9eba289c85b5a4fde882f9c6f5a7403c905da2d
Anders Nilsson <anders.nilsson@intel.com>
2013-10-09 13:02:03 +0200

    Select i2c impl when INTEL_POWER_HAL_I2C is set
    INTEL_POWER_HAL_I2C enables the i2c Power HAL impl.
    Define TARGET PRODUCT specific i2c devices, that
    shall be powered down when non-interactive, in
    vendor/intel/hardware/libpower/i2c/$(TARGET_PRODUCT).
    
    Change-Id: I458d4188dd8b491886239a121ef69c3f7b316b73
    Signed-off-by: Anders Nilsson <anders.nilsson@intel.com>

--- i2c/Android.mk
+++ i2c/Android.mk
@@ -12,22 +12,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(INTEL_POWER_HAL_I2C),true)
+
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-common_src_path := ..
-common_src_files := $(common_src_path)/InputDevicePowerMonitor.cpp \
-		    $(common_src_path)/power.cpp \
 
 include external/stlport/libstlport.mk
-LOCAL_C_INCLUDES += $(LOCAL_PATH) ..
+LOCAL_C_INCLUDES += $(LOCAL_PATH) $(LOCAL_PATH)/$(TARGET_PRODUCT)
 
-LOCAL_MODULE := power.haswell
+LOCAL_MODULE := power.$(TARGET_PRODUCT)
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
-LOCAL_SRC_FILES := $(common_src_files) \
-                   InputDevicePowerMonitorInfo.cpp \
+LOCAL_SRC_FILES := InputDevicePowerMonitor.cpp \
+                   power.cpp \
+                   $(TARGET_PRODUCT)/InputDevicePowerMonitorInfo.cpp \
 
 LOCAL_SHARED_LIBRARIES := liblog libcutils libstlport
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+endif
