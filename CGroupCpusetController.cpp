e808fb81a96d13047b631a868842be8a97b85576
Zhang Ning <ning.a.zhang@intel.com>
2016-02-17 12:57:31 +0800

    [improvement] change cpuset path to /dev/
    due to AOSP's init.rc mount cpuset to /dev/cpuset
    so power HAL is doing releated changes
    
    Tracked-On: OAM-21209
    Change-Id: I0e5343f29326fcf1284d9da4de7b588f07a7c90e
    Signed-off-by: Zhang Ning <ning.a.zhang@intel.com>
    Reviewed-on: 473305

--- CGroupCpusetController.cpp
+++ CGroupCpusetController.cpp
@@ -30,8 +30,8 @@
 #include <errno.h>
 #include <string.h>
 
-static const char* CPUSET_ROOT_CPUS = "/sys/fs/cgroup/cpuset/cpus";
-static const char* CPUSET_NON_INTERACTIVE_CPUS = "/sys/fs/cgroup/cpuset/power_hal/non_interactive/cpus";
+static const char* CPUSET_ROOT_CPUS = "/dev/cpuset/cpus";
+static const char* CPUSET_NON_INTERACTIVE_CPUS = "/dev/cpuset/non_interactive/cpus";
 static const char* POWER_HAL_CPUSET_PROPERTY = "ro.powerhal.cpuset_config";
 static const char* POWER_HAL_CPUSET_PROPERTY_DEBUG = "persist.powerhal.cpuset_config"; /* for userdebug, eng build tuning*/
 
